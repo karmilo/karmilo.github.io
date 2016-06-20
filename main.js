bc2b067267fa40e5abcca31bbcd1d82a3d0638ea
miloye <miloye@163.com>
2016-06-20 12:21:34 +0800

    make it more batter
    自适应

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-const MARGIN_X = 100;
-const MARGIN_Y = 100;
-var r=6;
-var cx=MARGIN_X; //第一个数字位置
-var cy=MARGIN_Y;
+var winWidth;
+var winHeight;
+var MARGIN_X;
+var MARGIN_Y;
+var r;
+var cx; //第一个数字位置
+var cy;
 var lastTime=''; //标记时间是否改变
 var timeStatus=false;
 var addBall=false; //是否需要生成小球
 var balls=new Array(); //小球管理数组
 window.onload=function(){
+
 	window.canvas=document.getElementById('canvas');
 	window.context=canvas.getContext('2d');
+	canvas.width=document.body.clientWidth;
+	canvas.height=document.body.clientHeight;
 	
 	window.clockTimeManage=setInterval(function(){
+		//获取宽高
+		getWH();
+		canvas.width=Math.round(winWidth);
+		canvas.height=Math.round(winHeight);
+		r=Math.round(winWidth*3/7/108)-1;
+		MARGIN_X=Math.round(winWidth*2/7);
+		MARGIN_Y=Math.round(winHeight/5);
+		cx=MARGIN_X;
+		cy=MARGIN_Y;
 		//获取时间
 		var date=new Date();
 		var hour=makeTime(date.getHours());
@@ -22,11 +36,16 @@ window.onload=function(){
 		if(timeStr!=lastTime){
 			timeStatus=true;
 		}
-		makeBitmap(context,timeStr);
+		makeBitmap(timeStr);
 		ballsDrop();
 		timeStatus=false;
 	},30);
 };
+
+function getWH(){
+	if (window.innerWidth) winWidth = window.innerWidth; else if ((document.body) && (document.body.clientWidth)) winWidth = document.body.clientWidth;
+	if (window.innerHeight) winHeight = window.innerHeight; else if ((document.body) && (document.body.clientHeight)) winHeight = document.body.clientHeight;
+}
 
 function makeTime(num){
 	if(num<10){
@@ -36,37 +55,37 @@ function makeTime(num){
 	}
 }
 
-function makeBitmap(cxt,str){
-	cxt.clearRect(0,0,cxt.canvas.width,cxt.canvas.height);
-	cx=MARGIN_X;
+function makeBitmap(str){
+	context.clearRect(0,0,document.body.clientWidth,document.body.clientHeight);
+	window.cx=MARGIN_X;
 	for(var i=0;i<6;i++){
 		//查找不同的数字下标
 		if(timeStatus&&str.substr(i,1)!=lastTime.substr(i,1)){
 			addBall=true;
 		}
 		//开始绘制单个点阵图
-		makeNum(str.substr(i,1),cxt);
+		makeNum(str.substr(i,1));
 		cx+=8*(r+1)*2;
 		addBall=false;
 		//绘制冒号点阵图
 		if(i%2==1&&i<5){
-			makeNum(10,cxt);
+			makeNum(10);
 			cx+=4*(r+1)*2;
 		}
 	}
 	lastTime=str;
 }
 
-function makeNum(num,cxt){
+function makeNum(num){
 	for(var index in digit){
 		if(index==num){
 			for(var row in digit[index]){
 				for(var field in digit[index][row]){
 					if(digit[index][row][field]==1){
-						cxt.beginPath();
-						cxt.arc(cx+parseInt(field)*(r+1)*2,cy+parseInt(row)*(r+1)*2,r,0,2*Math.PI);
-						cxt.fillStyle="black";
-						cxt.fill();
+						context.beginPath();
+						context.arc(cx+parseInt(field)*(r+1)*2,cy+parseInt(row)*(r+1)*2,r,0,2*Math.PI);
+						context.fillStyle="black";
+						context.fill();
 						if(addBall){
 							//新建一个小球实例，添加到数组管理
 							if(Math.random()>0.5){
@@ -97,11 +116,11 @@ function Ball(x,y,vx,col){
 		context.beginPath();
 		this.y+=this.vy;
 		this.x+=this.vx;
-		if(this.y>=(canvas.height-(r+1)*2)){
-			this.y=canvas.height-(r+1)*2;
+		if(this.y>=(document.body.clientHeight-(r+1)*2)){
+			this.y=document.body.clientHeight-(r+1)*2;
 			this.vy=(-this.vy*(Math.random()*(0.2)+0.6)); //反弹
 		}
-		if(this.x>=(canvas.width-(r+1)*2)||this.x<=(r+1)*2){
+		if(this.x>=(document.body.clientWidth-(r+1)*2)||this.x<=(r+1)*2){
 			delete balls[key]; //优化
 		}
 		context.arc(this.x,this.y,r,0,2*Math.PI);

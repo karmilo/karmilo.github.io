385fc1cd789e61cd59e1eee0cc6b24ceafa15a8c
miloye <miloye@163.com>
2016-07-07 15:26:11 +0800

    添加音乐标题

--- js/miloye_a.js
+++ js/miloye_a.js
@@ -1,10 +1,9 @@
 var audio = document.querySelector('#audio');
+var title=document.querySelector('#title');
+title.innerHTML=music[audio.currentSrc.substr(audio.currentSrc.indexOf('.mp3')-1,1)];
 var url=audio.src.substr(0,audio.src.indexOf('.mp3')-1);
 audio.addEventListener("error", function(){
-    audio.src=url+'1.mp3';
-	audio.load();
-	audio.play();
-	play.querySelector('span').querySelector('span').className='icon-pause';
+	playMusic(url+'1.mp3');
 }, false);
 var play = document.querySelector('#play');
 play.onclick=function(){
@@ -21,18 +20,19 @@ next.onclick=function(){
 	var src=audio.src;
 	var fileName=src.substr(src.indexOf('.mp3')-1,1);
 	fileName++;
-	audio.src=url+fileName+'.mp3';
-	audio.load();
-	audio.play();
-	play.querySelector('span').querySelector('span').className='icon-pause';
+	playMusic(url+fileName+'.mp3');
 }
 var prev = document.querySelector('#prev');
 prev.onclick=function(){
 	var src=audio.src;
 	var fileName=src.substr(src.indexOf('.mp3')-1,1);
 	fileName--;
-	audio.src=url+fileName+'.mp3';
+	playMusic(url+fileName+'.mp3');
+}
+function playMusic(url){
+	audio.src=url;
 	audio.load();
 	audio.play();
+	title.innerHTML=music[audio.src.substr(audio.src.indexOf('.mp3')-1,1)];
 	play.querySelector('span').querySelector('span').className='icon-pause';
 }
---
+++ js/music.js
@@ -0,0 +1,5 @@
+var music={
+	1:'Chris Medina - What Are Words',
+	2:'Dj Okawari - Luv Letter',
+	3:'筷子兄弟 - 父亲'
+}

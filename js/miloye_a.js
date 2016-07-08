64c214294611e618cae964ec5ff83050e83aefb9
miloye <miloye@163.com>
2016-07-08 22:35:25 +0800

    make it better

--- js/miloye_a.js
+++ js/miloye_a.js
@@ -2,9 +2,11 @@ var audio = document.querySelector('#audio');
 var title=document.querySelector('#title');
 title.innerHTML=music[audio.currentSrc.substr(audio.currentSrc.indexOf('.mp3')-1,1)];
 var url=audio.src.substr(0,audio.src.indexOf('.mp3')-1);
+/* 添加错误监听器，目的为了防止切换音乐时文件错误
 audio.addEventListener("error", function(){
 	playMusic(url+'1.mp3');
 }, false);
+*/
 var play = document.querySelector('#play');
 play.onclick=function(){
 	if(audio.paused){
@@ -20,14 +22,22 @@ next.onclick=function(){
 	var src=audio.src;
 	var fileName=src.substr(src.indexOf('.mp3')-1,1);
 	fileName++;
-	playMusic(url+fileName+'.mp3');
+	if(music[fileName]){
+		playMusic(url+fileName+'.mp3');
+	}else{
+		playMusic(url+'1.mp3');
+	}
 }
 var prev = document.querySelector('#prev');
 prev.onclick=function(){
 	var src=audio.src;
 	var fileName=src.substr(src.indexOf('.mp3')-1,1);
 	fileName--;
-	playMusic(url+fileName+'.mp3');
+	if(music[fileName]){
+		playMusic(url+fileName+'.mp3');
+	}else{
+		playMusic(url+'1.mp3');
+	}
 }
 function playMusic(url){
 	audio.src=url;

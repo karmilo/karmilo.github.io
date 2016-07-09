8bcf18674c2aceb4c47667ffa7f78a3460f77666
miloye <miloye@163.com>
2016-07-10 00:47:56 +0800

    PHP技术文档

---
+++ js/miloye_php.js
@@ -0,0 +1,7 @@
+var content = document.querySelectorAll('.content-section');
+var nav = document.querySelector('#nav');
+var lis='';
+for(var k=0;k<content.length;k++){
+	lis = lis +'<li><a href="#'+content[k].id+'"><i class="icon-chevron-right"></i> '+(content[k].querySelector('h1').innerHTML)+'</a></li>';
+}
+nav.innerHTML=lis+'<a href="#F1">返回顶部</a>';

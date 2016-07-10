b976c17bb63c3a36f04e56a992dd44e2548eaeac
miloye <miloye@163.com>
2016-07-10 23:47:34 +0800

    php技术文档
    会话控制

--- js/miloye_php.js
+++ js/miloye_php.js
@@ -4,4 +4,4 @@ var lis='';
 for(var k=0;k<content.length;k++){
 	lis = lis +'<li><a href="#'+content[k].id+'"><i class="icon-chevron-right"></i> '+(content[k].querySelector('h1').innerHTML)+'</a></li>';
 }
-nav.innerHTML=lis+'<a href="#F1">返回顶部</a>';
+nav.innerHTML=lis+'<a href="#top">返回顶部</a>';
